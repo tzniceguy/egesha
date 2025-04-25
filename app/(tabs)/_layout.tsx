4b72d75c4452a04115959575172a78aee7e9db96
tzniceguy <joeshimbi@gmail.com>
2025-04-25 13:34:57 +0300

    refactor: wrapping the screens with gesture handler for gestures

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,44 +1,47 @@
 import { Tabs } from "expo-router";
 import { Home, Calendar, User2 } from "lucide-react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const TabsLayout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "#4CAF50",
-        tabBarInactiveTintColor: "#757575",
-        tabBarStyle: {
-          backgroundColor: "#fff",
-          height: 72,
-          left: 10,
-          right: 10,
-          padding: 10,
-        },
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => <Home size={24} color={color} />,
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#4CAF50",
+          tabBarInactiveTintColor: "#757575",
+          tabBarStyle: {
+            backgroundColor: "#fff",
+            height: 72,
+            left: 10,
+            right: 10,
+            padding: 10,
+          },
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="bookings"
-        options={{
-          title: "Bookings",
-          tabBarIcon: ({ color }) => <Calendar size={24} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "account",
-          tabBarIcon: ({ color }) => <User2 size={24} color={color} />,
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => <Home size={24} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="bookings"
+          options={{
+            title: "Bookings",
+            tabBarIcon: ({ color }) => <Calendar size={24} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "account",
+            tabBarIcon: ({ color }) => <User2 size={24} color={color} />,
+          }}
+        />
+      </Tabs>
+    </GestureHandlerRootView>
   );
 };
 
