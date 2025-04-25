c911047717c6f56d80a983996053f3e43dc909b3
tzniceguy <joeshimbi@gmail.com>
2025-04-25 15:47:36 +0300

    feat: add booking detail page to stack navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="bookings/[id]" options={{ headerShown: false }} />
     </Stack>
   );
 }
