c9c69f4135d536c48a284b8868ee665c4270ffd7
joe <joeshimbi@gmail.com>
2025-07-01 17:48:07 +0300

    chore: add protection to routes to be accesible to auth users only

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
 import { Stack } from "expo-router";
 
+const isLoggedin = false;
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="bookings/[id]" options={{ headerShown: false }} />
+      <Stack.Protected guard={isLoggedin}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Protected guard={!isLoggedin}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="bookings/[id]" options={{ headerShown: false }} />
+      </Stack.Protected>
     </Stack>
   );
 }
