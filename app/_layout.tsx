eacf7f9d87057724282008918f005afe34eb5209
joe <joeshimbi@gmail.com>
2025-07-18 04:30:55 +0300

    chore(dependecy): add tanstack query for data fetching

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,9 @@ import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useAuthStore } from "@/stores/auth";
 import { useEffect } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const { isAuthenticated, initialize } = useAuthStore();
@@ -11,17 +14,22 @@ export default function RootLayout() {
   }, [initialize]);
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack>
-        <Stack.Protected guard={!isAuthenticated}>
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        </Stack.Protected>
-        <Stack.Protected guard={isAuthenticated}>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="bookings/[id]" options={{ headerShown: false }} />
-          <Stack.Screen name="vehicles" options={{ headerShown: false }} />
-        </Stack.Protected>
-      </Stack>
-    </GestureHandlerRootView>
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Protected guard={!isAuthenticated}>
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          </Stack.Protected>
+          <Stack.Protected guard={isAuthenticated}>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="bookings/[id]"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="vehicles" options={{ headerShown: false }} />
+          </Stack.Protected>
+        </Stack>
+      </GestureHandlerRootView>
+    </QueryClientProvider>
   );
 }
